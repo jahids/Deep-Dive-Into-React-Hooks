51fadf92c5a3d4221b3641a07180ba6f45281446
jahids <evanjahid321@gmail.com>
2022-11-09 18:28:21 +0600

    changes name

--- src/component/HookApplication.jsx
+++ src/component/HookApplication.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import UseCounter from "../Hook/UseCounter";
 
-const CounterReusuble = ({ count, increment, decrement }) => {
+const CounterReusable = ({ count, increment, decrement }) => {
   return (
     <div>
       <button onClick={increment}>+</button>
@@ -18,17 +18,17 @@ const HookApplication = () => {
   const count3 = UseCounter({ initial: 0, max: 6, min: 4 });
   return (
     <div>
-      <CounterReusuble
+      <CounterReusable
         count={count1.count}
         increment={count1.handleIncrement}
         decrement={count1.handleDecrement}
       />
-      <CounterReusuble
+      <CounterReusable
         count={count2.count}
         increment={count2.handleIncrement}
         decrement={count2.handleDecrement}
       />
-      <CounterReusuble
+      <CounterReusable
         count={count3.count}
         increment={count3.handleIncrement}
         decrement={count3.handleDecrement}
